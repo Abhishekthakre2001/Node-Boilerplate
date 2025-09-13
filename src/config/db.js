bdeac75b4d424cdbb4e6475ccb9d7dca98b3df36
Abhishek Thakre <abhishekpthakre2023@gmail.com>
2025-09-13 22:45:58 +0530

    DB config chnages

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 // ✅ MySQL Connection Pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "Abhishek@123",
-  database: process.env.DB_NAME || "employee_db",
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10, // number of parallel DB connections
   queueLimit: 0,
